d9badf9a67413f0f99397690627716e6453e7242
Nati Gurevich <nati_g4@hotmail.com>
2021-12-29 16:35:11 +0200

    styling and table fixes

--- js/apps/mail/cmps/EmailList.jsx
+++ js/apps/mail/cmps/EmailList.jsx
@@ -5,12 +5,13 @@ export function EmailList({ mails }) {
   if (!mails.length) return <h1> Inbox is empty... </h1>;
   return (
     <section className="mails-list grid">
+        <div className="flex">
       <h6>CheckBox</h6>
       <h6>Star</h6>
       <h6>Sender</h6>
       <h6>Subject</h6>
       <h6>Date</h6>
-
+      </div>
       {mails.map((mail) => (
         <EmailPreview key={mail.id} mail={mail} />
       ))}
--- js/apps/mail/cmps/EmailPreview.jsx
+++ js/apps/mail/cmps/EmailPreview.jsx
@@ -14,14 +14,12 @@ export class EmailPreview extends React.Component {
   render() {
     const { mail } = this.props;
     return (
-      <Link to={`/book/${mail.id}`}>
-        <div className="mail-preview">
+      <Link className="mail-preview-container flex" to={`/book/${mail.id} `}>
           <h6>CheckBox</h6>
           <h6>Star</h6>
           <h6>{mail.subject}</h6>
           <h6>{mail.body}</h6>
           <h6>{mail.to}</h6>
-        </div>
       </Link>
     );
   }
