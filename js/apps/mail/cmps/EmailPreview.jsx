9170618f6fcfd839bfdbbbde6133a8e34a6da021
Nati Gurevich <nati_g4@hotmail.com>
2021-12-29 21:22:55 +0200

    stars

--- js/apps/mail/cmps/EmailPreview.jsx
+++ js/apps/mail/cmps/EmailPreview.jsx
@@ -1,51 +1,61 @@
-import { emailService } from "../services/email.service.js";
-import {DangerButton} from '../../../../cmps/DangerButton.jsx';
+import { emailService } from '../services/email.service.js';
+import { DangerButton } from '../../../../cmps/DangerButton.jsx';
 
 const { Link } = ReactRouterDOM;
 
 export class EmailPreview extends React.Component {
-    state = {
-        isClicked: false,
-    };
+  state = {
+    isClicked: false,
+  };
 
-    extandMailView = () => {
-        const { isClicked } = this.state;
-        this.props.mail.isRead = true;
-        !isClicked ? this.setState({ isClicked: true }) : this.setState({ isClicked: false });
-    }
+  extandMailView = () => {
+    const { isClicked } = this.state;
+    this.props.mail.isRead = true;
+    !isClicked
+      ? this.setState({ isClicked: true })
+      : this.setState({ isClicked: false });
+  };
 
-    replyToMail = () => {
-        console.log('Reply to mail');
-    }
+  replyToMail = () => {
+    console.log('Reply to mail');
+  };
 
-    deleteMail = () => {
-        const {mail} = this.props;
-        emailService.remove(mail.id).then(this.props.loadMails);
-    }
+  deleteMail = () => {
+    const { mail } = this.props;
+    emailService.remove(mail.id).then(this.props.loadMails);
+  };
 
-    render() {
-        const { mail } = this.props;
-        const { isClicked } = this.state;
-        return (
-            <div className="mail-preview-container flex" onClick={this.extandMailView}>
-                {!isClicked && <div className="short-mail-view flex">
-                    <h6>CheckBox</h6>
-                    <h6>Star</h6>
-                    <h6>{mail.subject}</h6>
-                    <h6>{mail.body}</h6>
-                    <h6>{mail.to}</h6>
-                </div>}
-                {isClicked && <div className="long-mail-view grid">
-                    <div className="long-mail-btn flex">
-                        <Link to={`/mail/${mail.id}`}><button>❏</button></Link>
-                        <button onClick={this.replyToMail}>reply</button>
-                        <DangerButton func={this.deleteMail} txt="Delete"/>
-                    </div>
-                    <h4 className="subject">{mail.subject}</h4>
-                    <h6 className="to">{mail.to}</h6>
-                    <h6 className="mail-body" >{mail.body}</h6>
-                </div>}
+  render() {
+    const { mail } = this.props;
+    const { isClicked } = this.state;
+    return (
+      <div
+        className="mail-preview-container flex"
+        onClick={this.extandMailView}>
+        {!isClicked && (
+          <div className="short-mail-view flex">
+            <h6>CheckBox</h6>
+            <h6 className="star">&#9733;</h6>
+            <h6>{mail.subject}</h6>
+            <h6>{mail.body}</h6>
+            <h6>{mail.to}</h6>
+          </div>
+        )}
+        {isClicked && (
+          <div className="long-mail-view grid">
+            <div className="long-mail-btn flex">
+              <Link to={`/mail/${mail.id}`}>
+                <button>❏</button>
+              </Link>
+              <button onClick={this.replyToMail}>reply</button>
+              <DangerButton func={this.deleteMail} txt="Delete" />
             </div>
-        );
-    }
+            <h4 className="subject">{mail.subject}</h4>
+            <h6 className="to">{mail.to}</h6>
+            <h6 className="mail-body">{mail.body}</h6>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
