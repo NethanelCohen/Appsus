3908c2378b27e9e024c0d41f135e951942d1a08f
NetanelCohen <ncforb@gmail.com>
2021-12-29 16:35:46 +0200

    working on preview

--- js/apps/mail/cmps/EmailPreview.jsx
+++ js/apps/mail/cmps/EmailPreview.jsx
@@ -7,22 +7,37 @@ export class EmailPreview extends React.Component {
     isClicked: false,
   };
 
-  handleRowClick = () => {
-    this.setState({ isClicked: true });
-  };
+  extandMailView = () => {
+    const {isClicked} = this.state;
+    !isClicked ? this.setState({ isClicked: true }) : this.setState({ isClicked: false });
+  }
+
+  handleChange = () => {
+      console.log(this.props.history);
+    // this.props.history.push(`/mail/${mail.id}`);
+  }
 
   render() {
     const { mail } = this.props;
+    const {isClicked} = this.state;
     return (
-      <Link to={`/book/${mail.id}`}>
-        <div className="mail-preview">
+        <div className="mail-preview" onClick={this.extandMailView}>
+          {!isClicked && <div className="short-mail-view">
           <h6>CheckBox</h6>
           <h6>Star</h6>
           <h6>{mail.subject}</h6>
           <h6>{mail.body}</h6>
           <h6>{mail.to}</h6>
+        </div>}
+        {isClicked && <div className="long-mail-view">
+          <h4>{mail.subject}</h4>
+          <div className="long-mail-btn">
+              <button onClick={this.handleChange}>❏</button>
+              </div>
+          <h6>{mail.to}</h6>
+          <h6>{mail.body}</h6>
+        </div>}
         </div>
-      </Link>
     );
   }
 }
