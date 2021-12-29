eb285d5b51713ba8383bd9cefcfd391f9dc4d8b0
Nati Gurevich <nati_g4@hotmail.com>
2021-12-30 01:02:58 +0200

    finishes

--- js/apps/mail/cmps/Folders.jsx
+++ js/apps/mail/cmps/Folders.jsx
@@ -2,7 +2,6 @@ export function Folders() {
   return (
     <section className="email-folder-list">
       <ul className="flex column" onClick={() => console.log(event.target.dataset.value)}>
-        <li data-value="all">All</li>
         <li data-value="inbox">Inbox</li>
         <li data-value="sent">Sent</li>
         <li data-value="trash">Trash</li>
