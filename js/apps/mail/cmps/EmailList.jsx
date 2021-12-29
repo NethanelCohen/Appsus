88f39796fb8cfa33c2b8f90f61c99b4b1d05d769
Nati Gurevich <nati_g4@hotmail.com>
2021-12-29 15:12:19 +0200

    table

--- js/apps/mail/cmps/EmailList.jsx
+++ js/apps/mail/cmps/EmailList.jsx
@@ -1,12 +1,25 @@
+import { EmailPreview } from '../cmps/EmailPreview.jsx';
 
-import {EmailPreview} from '../cmps/EmailPreview.jsx'
+export function EmailList({ mails }) {
+  console.log(mails);
+  if (!mails.length) return <h1> Inbox is empty... </h1>;
+  return (
+    <section className="mails-list grid">
+      <table>
+        <thead>
+          <tr>
+            <th>CheckBox</th>
+            <th>Star</th>
+            <th>Sender</th>
+            <th>Subject</th>
+            <th>Date</th>
+          </tr>
+        </thead>
 
-export function EmailList({mails}) {
-    console.log(mails);
-    if (!mails.length) return <h1> Inbox is empty... </h1>
-    return (
-        <section className='mails-list grid'>
-            {mails.map(mail => <EmailPreview key={mail.id} mail={mail} />)}
-        </section>
-    )
+        <tbody>
+          {mails.map((mail) => <EmailPreview key={mail.id} mail={mail} />)}
+         </tbody>
+      </table> 
+         </section>
+  );
 }
--- js/apps/mail/cmps/EmailPreview.jsx
+++ js/apps/mail/cmps/EmailPreview.jsx
@@ -1,15 +1,19 @@
-
-const { Link } = ReactRouterDOM
+const { Link } = ReactRouterDOM;
 
 export function EmailPreview({ mail }) {
-    console.log(mail);
-    return (
-        <Link to={`/mail/${mail.id}`}>
-            <article className="mails-preview flex">
-                <h4>{mail.subject}</h4>
-                <h6>{mail.body}</h6>
-                <h6>{mail.to}</h6>
-            </article>
-        </Link>
-    )
+    // let isClicked= false;
+
+    function handleRowClick(id){
+        // <Link to={`/mail/${id}`}></Link>
+        console.log(id)
+    }
+  return (
+    <tr className="mail-preview" onClick={()=>handleRowClick(mail.id)} >
+      <td>CheckBox</td>
+      <td>Star</td>
+      <td>{mail.subject}</td>
+      <td>{mail.body}</td>
+      <td>{mail.to}</td>
+    </tr>
+  );
 }
