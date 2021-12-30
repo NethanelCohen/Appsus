46c74a831f0a7d453652505538d027454dec5c9f
Nati Gurevich <nati_g4@hotmail.com>
2021-12-30 12:14:23 +0200

    is starred

--- js/apps/mail/cmps/EmailPreview.jsx
+++ js/apps/mail/cmps/EmailPreview.jsx
@@ -6,20 +6,33 @@ const { Link } = ReactRouterDOM;
 export class EmailPreview extends React.Component {
   state = {
     isClicked: false,
-    isMouseOver: false
+    isMouseOver: false,
   };
 
   handleOpenMail = () => {
     const mail = this.props;
-    emailService.mailIsRead(mail.id).then(this.setState({ isClicked: false }))
-  }
+    emailService.mailIsRead(mail.id).then(this.setState({ isClicked: false }));
+  };
 
-  extandMailView = () => {
-    const { id } = this.props.mail;
-    if(!id) return
+  extandMailView = (e) => {
+    // debugger;
     const { isClicked } = this.state;
-    emailService.mailIsRead(id).then(!isClicked ? this.setState({ isClicked: true }, this.props.loadMails) : this.setState({ isClicked: false }, this.props.loadMails))
-  }
+    if (e.target.className.includes('star')) {
+     ()=>this.props.setColorStar(e);
+     return
+   }
+    const { id } = this.props.mail;
+    if (!id) return;
+  
+    emailService
+      .mailIsRead(id)
+      .then(
+        !isClicked
+          ? this.setState({ isClicked: true }, this.props.loadMails)
+          : this.setState({ isClicked: false }, this.props.loadMails)
+      );
+      console.log(e.target)
+  };
 
   replyToMail = () => {
     console.log('Reply to mail');
@@ -27,19 +40,23 @@ export class EmailPreview extends React.Component {
 
   deleteMail = () => {
     const { mail } = this.props;
-    emailService.remove(mail.id).then(this.props.loadMails)
-  }
+    emailService.remove(mail.id).then(this.props.loadMails);
+  };
 
   handleDateCheck = (timestamp) => {
-    var date = new Date(timestamp).toLocaleDateString("en-US")
-    if (Date.now() - timestamp < 1000 * 60 * 60 * 24) date = 'Today at ' + new Date(timestamp).toLocaleTimeString("en-US");
-    else if (Date.now() - timestamp < 1000 * 60 * 60 * 24 * 2) date = 'Yesterday at ' + new Date(timestamp).toLocaleTimeString("en-US");
+    var date = new Date(timestamp).toLocaleDateString('en-US');
+    if (Date.now() - timestamp < 1000 * 60 * 60 * 24)
+      date = 'Today at ' + new Date(timestamp).toLocaleTimeString('en-US');
+    else if (Date.now() - timestamp < 1000 * 60 * 60 * 24 * 2)
+      date = 'Yesterday at ' + new Date(timestamp).toLocaleTimeString('en-US');
     return date;
   };
 
   handleMouse = (state) => {
-    state === 'on' ? this.setState({ isMouseOver: true }) : this.setState({ isMouseOver: false });
-  }
+    state === 'on'
+      ? this.setState({ isMouseOver: true })
+      : this.setState({ isMouseOver: false });
+  };
 
   render() {
     const { mail } = this.props;
@@ -50,9 +67,13 @@ export class EmailPreview extends React.Component {
     return (
       <div
         className="mail-preview-container flex"
-        onClick={this.extandMailView} onMouseOver={() => this.handleMouse('on')} onMouseLeave={() => this.handleMouse('off')}>
+        onClick={this.extandMailView}
+        onMouseOver={() => this.handleMouse('on')}
+        onMouseLeave={() => this.handleMouse('off')}>
         {!isClicked && (
-          <div style={{ backgroundColor: `${isMailRead}` }} className="short-mail-view flex">
+          <div
+            style={{ backgroundColor: `${isMailRead}` }}
+            className="short-mail-view flex">
             <h6>CheckBox</h6>
             <h6
               className={'star off'}
@@ -62,29 +83,43 @@ export class EmailPreview extends React.Component {
             <h6>{mail.subject}</h6>
             <h6>{mail.to}</h6>
             <h6>{mail.body}</h6>
-            {isMouseOver && <div className="hover-mail-btn flex">
-              <Link to={`/mail/${mail.id}`}>
-              <StyledButton func={this.handleOpenMail} txt="❏" bgc="grey" />
-              </Link>
-              <StyledButton func={this.replyToMail} txt="reply" bgc="green" />
-              <StyledButton func={this.deleteMail} txt="Delete" bgc="hsl(345deg 100% 47%)" />
-            </div>}
+            {isMouseOver && (
+              <div className="hover-mail-btn flex">
+                <Link to={`/mail/${mail.id}`}>
+                  <StyledButton func={this.handleOpenMail} txt="❏" bgc="grey" />
+                </Link>
+                <StyledButton func={this.replyToMail} txt="reply" bgc="green" />
+                <StyledButton
+                  func={this.deleteMail}
+                  txt="Delete"
+                  bgc="hsl(345deg 100% 47%)"
+                />
+              </div>
+            )}
             <h6>{date}</h6>
           </div>
         )}
-        {isClicked && <div style={{ backgroundColor: `${isMailRead}` }} className="long-mail-view grid">
-          <div className="long-mail-btn flex">
-            <Link to={`/mail/${mail.id}`}>
-            <StyledButton func={this.handleOpenMail} txt="❏" bgc="grey" />
-            </Link>
-             <StyledButton func={this.replyToMail} txt="reply" bgc="green" />
-            <StyledButton func={this.deleteMail} txt="Delete" bgc="hsl(345deg 100% 47%)"/>
+        {isClicked && (
+          <div
+            style={{ backgroundColor: `${isMailRead}` }}
+            className="long-mail-view grid">
+            <div className="long-mail-btn flex">
+              <Link to={`/mail/${mail.id}`}>
+                <StyledButton func={this.handleOpenMail} txt="❏" bgc="grey" />
+              </Link>
+              <StyledButton func={this.replyToMail} txt="reply" bgc="green" />
+              <StyledButton
+                func={this.deleteMail}
+                txt="Delete"
+                bgc="hsl(345deg 100% 47%)"
+              />
+            </div>
+            <h4 className="subject">{mail.subject}</h4>
+            <h6 className="to">{mail.to}</h6>
+            <h6>{date}</h6>
+            <h6 className="mail-body">{mail.body}</h6>
           </div>
-          <h4 className="subject">{mail.subject}</h4>
-          <h6 className="to">{mail.to}</h6>
-          <h6>{date}</h6>
-          <h6 className="mail-body">{mail.body}</h6>
-        </div>}
+        )}
       </div>
     );
   }
