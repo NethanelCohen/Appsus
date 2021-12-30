151b44e1166681761e83551f0dc3d701dd916128
NetanelCohen <ncforb@gmail.com>
2021-12-30 13:36:11 +0200

    input fixed

--- js/apps/mail/pages/EmailApp.jsx
+++ js/apps/mail/pages/EmailApp.jsx
@@ -22,6 +22,10 @@ export class EmailApp extends React.Component {
     this.setState((prevState) => ({ ...prevState, criteria: { ...prevState.criteria, status: newStatus } }), () => this.loadMails())
   }
 
+  handleCriteriaTxt = (newTxt) => {
+    this.setState((prevState) => ({ ...prevState, criteria: { ...prevState.criteria, txt: newTxt} }), () => this.loadMails())
+  }
+
 loadMails = () => {
     emailService.query(this.state.criteria).then((mails) => {
     this.setState({ mails });
@@ -29,11 +33,10 @@ loadMails = () => {
 };
 render() {
   const { mails } = this.state;
-  // if (!mails.length) return <h1>The inbox is empty</h1>;
   return (
     <div className="email-list-container grid">
       <div className="search-filter">
-        <input placeholder="Search mail"></input>
+        <input placeholder="Search mail" onChange={(ev) => this.handleCriteriaTxt(ev.target.value)}></input>
       </div>
       <Folders handleCriteriaStatus={this.handleCriteriaStatus} />
       <EmailList mails={mails} loadMails={this.loadMails} />
