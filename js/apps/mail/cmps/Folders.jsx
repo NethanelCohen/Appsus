8ce678e78b72e1884eb660520237b5c7c11a2d98
Nati Gurevich <nati_g4@hotmail.com>
2021-12-31 03:04:13 +0200

    show starred

--- js/apps/mail/cmps/Folders.jsx
+++ js/apps/mail/cmps/Folders.jsx
@@ -1,14 +1,39 @@
-
-
-export function Folders({handleCriteriaStatus,activeStatus}) {
+export function Folders({
+  handleCriteriaStatus,
+  activeStatus,
+  setStaredStatus,
+}) {
   return (
     <section className="email-folder-list">
-      <ul className="flex column" onClick={() => handleCriteriaStatus(event.target.dataset.value)}>
-        <li className={activeStatus === 'inbox' ? 'active-filter': ''} data-value="inbox">Inbox</li>
-        <li className={activeStatus === 'sent' ? 'active-filter': ''} data-value="sent">Sent</li>
-        <li className={activeStatus === 'trash' ? 'active-filter': ''}data-value="trash">Trash</li>
-        <li className={activeStatus === 'draft' ? 'active-filter': ''}data-value="draft">Draft</li>
+      <ul
+        className="flex column"
+        onClick={() => handleCriteriaStatus(event.target.dataset.value)}>
+        <li
+          className={activeStatus === 'inbox' ? 'active-filter' : ''}
+          data-value="inbox">
+          Inbox
+        </li>
+        <li
+          className={activeStatus === 'sent' ? 'active-filter' : ''}
+          data-value="sent">
+          Sent
+        </li>
+        <li
+          className={activeStatus === 'trash' ? 'active-filter' : ''}
+          data-value="trash">
+          Trash
+        </li>
+        <li
+          className={activeStatus === 'draft' ? 'active-filter' : ''}
+          data-value="draft">
+          Draft
+        </li>
+        <li
+          className={activeStatus === 'Starred' ? 'active-filter' : ''}
+          data-value="starred">
+          Starred
+        </li>
       </ul>
     </section>
-  )
+  );
 }
