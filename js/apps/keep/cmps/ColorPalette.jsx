20b048e942baeb97c372483659752ce2541a551a
Nati Gurevich <nati_g4@hotmail.com>
2022-01-02 22:08:37 +0200

    fix colors

--- js/apps/keep/cmps/ColorPalette.jsx
+++ js/apps/keep/cmps/ColorPalette.jsx
@@ -1,19 +1,19 @@
+export function ColorPalette({ note }) {
+  function setBackgroundColor({ target }) {
+    console.log(target);
+  }
 
-
-export function ColorPalette({note}) {
-
-    function setBackgroundColor({target}) {
-        console.log(target);
-    }
-
-    return (
-        <div className="note-color">
-            <input value={#D6E5FA} style={{backgroundColor: '#D6E5FA'}} onClick={(ev) => setBackgroundColor(ev)}></input>
-            <input style={{backgroundColor: '#BAABDA'}}></input>
-            <input style={{backgroundColor: '#FFEDB2'}}></input>
-            <input style={{backgroundColor: '#FFBF87'}}></input>
-            <input style={{backgroundColor: '#B8E9C0'}}></input>
-            <input style={{backgroundColor: '#EFB1FF'}}></input>
-        </div>
-    )
+  return (
+    <div className="note-color">
+      <input
+        value={'#D6E5FA'}
+        style={{ backgroundColor: '#D6E5FA' }}
+        onClick={(ev) => setBackgroundColor(ev)}></input>
+      <input style={{ backgroundColor: '#BAABDA' }}></input>
+      <input style={{ backgroundColor: '#FFEDB2' }}></input>
+      <input style={{ backgroundColor: '#FFBF87' }}></input>
+      <input style={{ backgroundColor: '#B8E9C0' }}></input>
+      <input style={{ backgroundColor: '#EFB1FF' }}></input>
+    </div>
+  );
 }
